5cd53bf7df4386f30d13182e32b48d895cb6c202
AntonKozlovskyy <Zeratul.85@mail.ru>
2014-05-08 21:26:12 +0300

    4

---
+++ Unity files/test_prj1 (2)/test_prj1/Assets/code/transpTriger.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class transpTriger : MonoBehaviour {
+
+
+	public string charName;
+	public string objName;
+	public float startTransp;
+	public float endTransp;
+	public float transpIt;
+
+	bool finished;
+	GameObject obj;
+	// Use this for initialization
+	void Start () {
+		obj=GameObject.Find(charName);
+		finished=false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(!finished&&transform.position.x<=obj.transform.position.x)
+		{
+			triger();
+		}
+	}
+
+	void triger()
+	{
+		GameObject transp=GameObject.Find(objName);
+		Color col=transp.transform.renderer.material.color;
+		col.a+=transpIt*Time.deltaTime;
+		if(transp.transform.renderer.material.color.a>=endTransp)
+		{
+			finished=true;
+		}
+	}
+}
